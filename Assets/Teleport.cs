dab471012be993e5c779fec8563e09a6aa46ad9c
Nick A. Spyrakos <nickpreveza@gmail.com>
2022-01-22 13:19:14 +0100

    Added Teleport while in vehicle

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -3,29 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour {
+    [SerializeField] FirstPersonMovement controller;
     private int _currentUniverse = 0;
     public bool canTeleport = true;
-
-    // Start is called before the first frame update
-    void Start()
+    public GameObject vehicleParent;
+    void Awake()
     {
-
+        controller = GetComponent<FirstPersonMovement>();
+        vehicleParent = transform.parent.transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //requires a validate method
         if (Input.GetKeyDown(KeyCode.T) && canTeleport) {
-            switch (_currentUniverse) {
-                case 0:
+            TeleportPlayer();   
+        }
+    }
+
+    void TeleportPlayer()
+    {
+        switch (_currentUniverse)
+        {
+            case 0:
+                if (controller.inVechile)
+                {
+
+                    vehicleParent.transform.position = new Vector3(vehicleParent.transform.position.x, vehicleParent.transform.position.y, vehicleParent.transform.position.z - 1000);
+                }
+                else
+                {
                     transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 200);
-                    _currentUniverse = 1;
-                    break;
-                case 1:
+                }
+              
+                _currentUniverse = 1;
+                break;
+            case 1:
+              
+                if (controller.inVechile)
+                {
+                    GameObject targetGameObject = this.transform.parent.transform.parent.gameObject;
+                    targetGameObject.transform.position = new Vector3(vehicleParent.transform.position.x, vehicleParent.transform.position.y, vehicleParent.transform.position.z + 1000);
+                }
+                else
+                {
                     transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 200);
-                    _currentUniverse = 0;
-                    break;
-            }
+                }
+                _currentUniverse = 0;
+                break;
         }
     }
 }
