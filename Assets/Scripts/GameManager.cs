ca0c829fbb322402310fd5d83a1eedb1d1d5f317
Nick A. Spyrakos <nickpreveza@gmail.com>
2022-01-22 15:07:47 +0100

    Auto stash before merge of "dev" and "origin/camera-and-controller"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool devMode;
+    public bool GodMode;
     // Start is called before the first frame update
     void Start()
     {
